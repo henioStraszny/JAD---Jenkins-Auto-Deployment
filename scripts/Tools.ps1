4e7d05b0f319772afdaa1b0a1a24cc8c2da713a1
henioStraszny <jacek.styrylski@gmail.com>
2016-04-20 13:30:50 +0200

    Added visualStudioCode to tools

--- scripts/Tools.ps1
+++ scripts/Tools.ps1
@@ -1,3 +1,4 @@
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install totalcmd -y
-choco install notepad++ -y
+choco install notepadplusplus -y
+choco install visualstudiocode -y
